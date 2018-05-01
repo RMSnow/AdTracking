3c600f8c89f21c0e8b7b231e3e35483ecae92a7a
snow <517606847@qq.com>
2018-05-01 23:45:54 +0800

    basic5: update

--- models/gbdt/predict.py
+++ models/gbdt/predict.py
@@ -18,9 +18,26 @@ dtypes = {
     'click_id': 'uint32'
 }
 
+print('loading train data...')
+train_df = pd.read_csv(path + "train_3750w.csv", dtype=dtypes,
+                       usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
+
+print('loading valid data...')
+val_df = pd.read_csv(path + "dev_250w.csv", dtype=dtypes,
+                     usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
+
+print('loading test data...')
+test_df = pd.read_csv(path + "test_250w.csv", dtype=dtypes,
+                      usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
+
 print('loading real world test data...')
 real_test_df = pd.read_csv(path + "test.csv", dtype=dtypes,
                            usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'click_id'])
+
+print("train size: ", len(train_df))
+print("valid size: ", len(val_df))
+print("test size : ", len(test_df))
+print("real world test size : ", len(real_test_df))
 
 
 # handle 'click_time'
@@ -29,6 +46,9 @@ def handle_click_time(df):
     df['day'] = pd.to_datetime(df.click_time).dt.day.astype('uint8')
 
 
+handle_click_time(train_df)
+handle_click_time(val_df)
+handle_click_time(test_df)
 handle_click_time(real_test_df)
 
 target = 'is_attributed'
@@ -37,15 +57,29 @@ target = 'is_attributed'
 predictors = ['ip', 'app', 'device', 'os', 'channel', 'hour', 'day']
 categorical = ['ip', 'app', 'device', 'os', 'channel', 'hour', 'day']
 
-sub = pd.DataFrame()
-sub['click_id'] = real_test_df['click_id'].astype('int')
+bst = lgb.Booster(model_file=path + 'model/model.txt')
 
-gc.collect()
+xgtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
+                          feature_name=predictors,
+                          categorical_feature=categorical
+                          )
+xgvalid = lgb.Dataset(val_df[predictors].values, label=val_df[target].values,
+                          feature_name=predictors,
+                          categorical_feature=categorical
+                          )
+xgtest = lgb.Dataset(test_df[predictors].values, label=test_df[target].values,
+                         feature_name=predictors,
+                         categorical_feature=categorical)
 
+bst.eval(xgtrain,'train')
+bst.eval(xgvalid,'valid')
+bst.eval(xgtest,'test')
+
+'''
 # Predict
-bst = lgb.Booster(model_file=path + 'model.txt')
 print("Predicting...")
 sub['is_attributed'] = bst.predict(real_test_df[predictors])
 print("writing...")
-sub.to_csv(path + 'sub_lgb_basic5.csv', index=False)
+sub.to_csv(path + 'sub/sub_lgb_basic5.csv', index=False)
 print("done...")
+'''
