c2b5664036e62b381190a844340e3631fb738e99
snow <517606847@qq.com>
2018-05-01 17:40:38 +0800

    preprocess: sampling finished

--- preprocess/dataset/sampling.py
+++ preprocess/dataset/sampling.py
@@ -77,3 +77,30 @@ operation = 'data_shuffled_4250w.csv'
 handle_operation(operation)
 sample_df.to_csv(path + operation)
 time_description(operation)
+
+'''
+/home/stu/.conda/envs/deep/bin/python /home/stu/Projects/Thdlee.Snow/TalkingData/preprocess/dataset/sampling.py
+当前时间为：2018-05-01 16:45:30，正在处理train.csv...
+成功处理 train.csv ,累计用时 233.386754 s
+
+当前时间为：2018-05-01 16:49:23，正在处理sample_df...
+成功处理 sample_df ,累计用时 258.572500 s
+
+当前时间为：2018-05-01 16:49:48，正在处理sorted_df...
+成功处理 sorted_df ,累计用时 339.554489 s
+
+当前时间为：2018-05-01 16:51:14，正在处理dev_250w.csv...
+成功处理 dev_250w.csv ,累计用时 357.362650 s
+
+当前时间为：2018-05-01 16:51:27，正在处理test_250w.csv...
+成功处理 test_250w.csv ,累计用时 370.246952 s
+
+当前时间为：2018-05-01 16:51:40，正在处理train_3750w.csv...
+成功处理 train_3750w.csv ,累计用时 554.877350 s
+
+当前时间为：2018-05-01 16:54:44，正在处理data_shuffled_4250w.csv...
+成功处理 data_shuffled_4250w.csv ,累计用时 776.042315 s
+
+
+Process finished with exit code 0
+'''
